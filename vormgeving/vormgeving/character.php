c940e002479485f40bf3f88af82c6d5de446bd48
Reza <99068159@mydavinci.nl>
2023-05-24 15:57:33 +0200

    Heb de character.php afgemaakt

--- vormgeving/vormgeving/character.php
+++ vormgeving/vormgeving/character.php
@@ -19,31 +19,26 @@
 <div id="container">
     <div class="detail">
         <div class="left">
-            <img class="avatar" src="resources/images/bowser.jpg">
-            <div class="stats" style="background-color: yellowgreen">
+            <img class="avatar" src="resources/images/<?= $alleCharacters[$id]['avatar']?>">
+            <div class="stats" style="background-color: <?= $alleCharacters[$id]['color']?>">
                 <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
-                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
-                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
+                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?= $alleCharacters[$id]['health']?></li>
+                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?= $alleCharacters[$id]['attack']?></li>
+                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?= $alleCharacters[$id]['defense']?></li>
                 </ul>
                 <ul class="gear">
-                    <li><b>Weapon</b>: Fire Breath</li>
-                    <li><b>Armor</b>: Giant Shell</li>
+                    <li><b>Weapon</b>: <?= $alleCharacters[$id]['weapon']?></li>
+                    <?php if ($alleCharacters[$id]['armor'] == NULL): ?>
+                        <li></li>
+                    <?php else: ?>
+                        <li><b>Armor</b>: <?= $alleCharacters[$id]['armor']?></li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </div>
         <div class="right">
             <p>
-                Bowser or King Koopa, is a fictional character and the main antagonist of Nintendo's Mario franchise. In
-                Japan, the character bears the title of Great Demon King. In the U.S., the character was first referred
-                to as "Bowser, King of the Koopas" and "the sorcerer king" in the instruction manual.<br/>
-                <br/>
-                Bowser is the leader of the turtle-like Koopa race, and has been the archenemy of Mario since his first
-                appearance, in the 1985 video game Super Mario Bros.<br/>
-                <br/>
-                His ultimate goals are to kidnap Princess Peach, defeat Mario, and conquer the Mushroom Kingdom. Since
-                his debut, he has appeared in almost every Mario franchise game, usually serving as the main antagonist.
-                Bowser is voiced by Kenny James.
+                <?= $alleCharacters[$id]['bio']?>
             </p>
         </div>
         <div style="clear: both"></div>
